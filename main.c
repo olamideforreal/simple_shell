193ba74b6b4ab0f2fe5259d0de9721eee19fd35e
olamideforreal <colamide2@gmail.com>
2022-05-18 18:29:22 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * main - starting point of the shell.
+ *Return: 0
+ */
+
+int main(void)
+{
+	char *user_input;
+	char **tokens;
+	char *oldpwd = (char *)malloc(sizeof(char) * 1024);
+	char *newpwd = (char *)malloc(sizeof(char) * 1024);
+
+	if (!oldpwd || !newpwd)
+		return (0);
+
+	while (1)
+	{
+		_putchar('$');
+		_putchar(' ');
+		user_input = get_user_input(oldpwd, newpwd);
+		if (user_input && (user_input[0] == '\n' || user_input[0] == '\0'))
+		{
+			continue;
+		}
+		else if (user_input != NULL)
+		{
+			tokens = tokenaization(user_input, oldpwd, newpwd);
+			if (tokens != NULL)
+			{
+			_stat(tokens, oldpwd, newpwd);
+			}
+		}
+	}
+	return (0);
+}
